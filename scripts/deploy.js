b3ffbc3c47a4bc457960e86cf1d14b1f624d53aa
Sunil Reddy <sunday7637@gmail.com>
2023-12-25 10:16:23 +0530

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,28 +6,33 @@ async function main() {
   // const simpleStorage = await contractFactory.deploy();
   // await simpleStorage.deployed();
   // console.log(`simple storage deployed: ${simpleStorage.address}` )
-  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage")  
-  const simpleStorage= await SimpleStorage.deploy();
+  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy();
   await simpleStorage.deployed();
-  console.log(`simple storage deployed: ${simpleStorage.address}` )
+  console.log(`simple storage deployed: ${simpleStorage.address}`);
 
-  console.log("deployed successfully🩵🩵")
+  console.log("deployed successfully🩵🩵");
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`current value is: ${currentValue}`);
+  await trasectionResponse.wait(1);
+  const updatedValue = await simpleStorage.store(69);
+  console.log(`current value is: ${updatedValue}`);
+
 }
 
-async function verify(contractAddress ,args){
+async function verify(contractAddress, args) {
   console.log("verify contract....");
   try {
-    await run("verify:verify",{
-      address:contractAddress,
-      constructorArguments:args
-    })
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
   } catch (error) {
     if (error.message.toLowerCase().includes("already verified")) {
-      console.log("already verified you mother f***** ....")
-      
+      console.log("already verified you mother f***** ....");
     }
   }
-
 }
 main()
   .then(() => process.exit(0))
